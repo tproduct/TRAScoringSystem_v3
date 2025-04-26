1dfe5174a85311c0722144cbf680a19cc91298bf
tproduct <tetsu.koba.tramp@gmail.com>
2025-04-26 17:31:29 +0900

    スタートリスト完成

--- src/components/pages/startlist/StartListPage.jsx
+++ src/components/pages/startlist/StartListPage.jsx
@@ -9,26 +9,23 @@ import { typeLabels, genderLabels, roundLabels } from "@libs/constants";
 
 const StartListPage = () => {
   const { competitionId, type, gender, categoryId, round } = useParams();
-  const userId = useSelector((state) => state.user.info.id);
   const { competition, fetchCompetition } = useCompetition(competitionId);
 
-  const [order, setOrder] = useState(null);
-
   useEffect(() => {
-    fetchOrder();
+    document.body.style.overflow = "auto";
+    document.documentElement.style.overflow = "auto";
     fetchCompetition();
+
+    return () => {
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
+    };
   }, []);
 
-  const fetchOrder = async () => {
-    const getOrder = useApiRequest(
-      `users/${userId}/competitions/${competitionId}/orders/${type}/${gender}/${categoryId}/${round}`
-    ).get;
-
-    const response = await getOrder();
-    if (response.status === "success") setOrder(response.data);
-  };
-
-  console.log(order);
+  if( !competition ) return null;
+  
+  const order = competition?.orders[type][gender][round][categoryId];
+  const players = competition?.players[type].find( categorizedPlayers => categorizedPlayers.category_id === categoryId)?.players;
 
   return (
     <Stack p="2">
@@ -49,6 +46,16 @@ const StartListPage = () => {
             <Table.ColumnHeader>Team</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
+        <Table.Body>
+          {order[0]?.map( orderedPlayers => (
+            <Table.Row key={orderedPlayers.id}>
+              <Table.Cell>{Math.floor(orderedPlayers.number / 100)}</Table.Cell>
+              <Table.Cell>{orderedPlayers.number % 100}</Table.Cell>
+              <Table.Cell>{players?.find( player => player.id === orderedPlayers.player_id)?.name + (type === "syncronized" ? "/"+players?.find( player => player.id === orderedPlayers.player_id)?.name2 : "")}</Table.Cell>
+              <Table.Cell>{players?.find( player => player.id === orderedPlayers.player_id)?.team + (type === "syncronized" ? "/"+players?.find( player => player.id === orderedPlayers.player_id)?.team2 : "")}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
       </Table.Root>
     </Stack>
   );
